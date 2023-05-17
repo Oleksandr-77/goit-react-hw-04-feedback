c59adcaf16c73b0b32b8625164beaf496e6e5106
Oleksandr Kokhno <kohnoan@gmail.com>
2023-05-18 00:11:55 +0300

    copy

--- src/components/components/App.jsx
+++ src/components/components/App.jsx
@@ -60,4 +60,19 @@ class App extends React.Component {
 export { App };
 
 
-
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
